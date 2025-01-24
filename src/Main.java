8d0dacc09596cefa1a22b675e63ff509a165c18d
Andrey Borov <lg700444@gmail.com>
2025-01-24 20:40:02 +0300

    Add regex link check

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,10 @@ public class Main {
         car.setAuthor("Andreuyyyuuyuy");
         car.setAuthor("Andreuyyyuuyuy");
         String str = "lg700444@gmail.com";
+        String str1 = "https://google.com";
         boolean isEmail = str.matches("[a-zA-z0-9]+@[a-zA-z0-9]+\\.(com|ru)");
+        boolean isLink = str1.matches("http[s]*://[a-zA-z0-9]+\\.(com|ru)");
         System.out.println(isEmail);
+        System.out.println(isLink);
     }
 }
